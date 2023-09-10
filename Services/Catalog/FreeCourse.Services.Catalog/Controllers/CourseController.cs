7d1d2f76ab12196b299e5413beea554c0b3a41f3
Doğukan Demir <doguka.demir991@gmail.com>
2023-09-10 23:01:33 +0300

    CourseController done

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -0,0 +1,62 @@
+﻿using FreeCourse.Services.Catalog.DTOs;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CourseController : CustomControllerBase
+{
+    private readonly ICourseService _courseService;
+
+    public CourseController(ICourseService courseService)
+    {
+        _courseService = courseService;
+    }
+
+    // course/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var response = await _courseService.GeyByIdAsync(id);
+        return CreateActionResultInstance(response);
+    }
+    
+    // course/GetAllByUser/5
+    [Route("/api/[controller]/GetAllByUserId/{userId}")]
+    public async Task<IActionResult> GetByUserId(string userId)
+    {
+        var response = await _courseService.GeyAllByUserId(userId);
+        return CreateActionResultInstance(response);
+    }
+    
+    public async Task<IActionResult> GetAll()
+    {
+        var response = await _courseService.GetAllAsync();
+        return CreateActionResultInstance(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CourseCreateDTO request)
+    {
+        var response = await _courseService.CreateAsync(request);
+        return CreateActionResultInstance(response);
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> Update(CourseUpdateDTO request)
+    {
+        var response = await _courseService.UpdateAsync(request);
+        return CreateActionResultInstance(response);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Update(string id)
+    {
+        var response = await _courseService.DeleteAsync(id);
+        return CreateActionResultInstance(response);
+    }
+    
+}
---
+++ Shared/FreeCourse.Shared/ControllerBase/CustomControllerBase.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Shared.ControllerBase
+{
+    public class CustomControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase 
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
