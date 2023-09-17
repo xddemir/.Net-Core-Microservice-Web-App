228f778f47666a4790e40fed9520b3c93a43525f
Doğukan Demir <doguka.demir991@gmail.com>
2023-09-17 15:30:21 +0300

    Automapper Added

---
+++ Services/Order/FreeCourse.Services.Order.Application/DTOs/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Services.Order.Domain.OrderAggregate;
+
+namespace FreeCourse.Services.Order.Application.DTOs;
+
+public class OrderDto
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get;  set; }
+    public AddressDto Address { get; set; }
+    public string BuyerId { get; set; }
+    public List<OrderItemDto> OrderItems { get; set; }
+}
